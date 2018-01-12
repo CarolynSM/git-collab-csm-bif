56189da29524bf5693770491d9674bd5b8f4ab82
Patrick Biffle <pbiffle@gmail.com>
2018-01-12 11:09:42 -0700

    added an array of numbers and a function to sum them

--- app/app.js
+++ app/app.js
@@ -1,14 +1,22 @@
 var doughnuts = [
-  {
-    name: Glazed,
-    age: 2
-  },
-  {
-    name: Chocolate,
-    age: 4
-  },
-  {
-    name: Jelly,
-    age: 14
-  }
-];
+	{
+		name: Glazed,
+		age: 2
+	},
+	{
+		name: Chocolate,
+		age: 4
+	},
+	{
+		name: Jelly,
+		age: 14
+	}
+]
+
+var arrayOfNumbers = [2, 1, 4, 5, 5, 1, 2, 3, 1, 2, 2, 56, 6, 6, 1, 1, 2, 2, 3, 3]
+
+function sum(array) {
+	return array.reduce((a, b) => {
+		return a + b
+	})
+}
