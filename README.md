70ea6d4b3426b334fd0ae4113f7814b88c00f812
Carolyn Selheim <carolyn.selheim@gmail.com>
2018-01-12 13:43:01 -0700

    created a function that multiplies numbers

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ You should edit five CSS properties.
 ### Task 6 - Patrick
 
 You should create an array of objects. Each object should have a name and age.
+
+### Task 7 - Carolyn
+
+You should create an array of numbers in a Javascript file, if there isn't one. Write a function that multiplies them together.
--- app/app.js
+++ app/app.js
@@ -1,47 +1,53 @@
 var doughnuts = [
-	{
-		name: Glazed,
-		age: 2
-	},
-	{
-		name: Chocolate,
-		age: 4
-	},
-	{
-		name: Jelly,
-		age: 14
-	}
-]
+  {
+    name: Glazed,
+    age: 2
+  },
+  {
+    name: Chocolate,
+    age: 4
+  },
+  {
+    name: Jelly,
+    age: 14
+  }
+];
 
-var arrayOfNumbers = [2, 1, 4, 5, 5, 1, 2, 3, 1, 2, 2, 56, 6, 6, 1, 1, 2, 2, 3, 3]
+var arrayOfNumbers = [2, 1, 4, 5, 5, 1, 2, 3, 1, 2, 2, 56, 6, 6, 1, 1, 2, 2, 3, 3];
 
 function sum(array) {
-	return array.reduce((a, b) => {
-		return a + b
-	})
+  return array.reduce((a, b) => {
+    return a + b;
+  });
 }
 
 function returnEven(array) {
-	var output = []
-	array.forEach(n => {
-		if (n % 2 === 0) {
-			output.push(n)
-		}
-	})
-	return output
+  var output = [];
+  array.forEach(n => {
+    if (n % 2 === 0) {
+      output.push(n);
+    }
+  });
+  return output;
 }
 
 var nuts = [
-	{
-		name: Chest,
-		age: 23
-	},
-	{
-		name: Pea,
-		age: 2
-	},
-	{
-		name: Dough,
-		age: 13
-	}
-]
+  {
+    name: Chest,
+    age: 23
+  },
+  {
+    name: Pea,
+    age: 2
+  },
+  {
+    name: Dough,
+    age: 13
+  }
+];
+
+function multiplyNumbers(array) {
+  return array.reduce(function(a, b) {
+    return a * b;
+  });
+}
